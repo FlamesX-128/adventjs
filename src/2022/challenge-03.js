735c557e64f713d9f684b155f8b24046b2f84b50
Flames <78381898+FlamesX-128@users.noreply.github.com>
2022-12-04 17:57:22 -0600

    Update challenge-03.js

--- src/2022/challenge-03.js
+++ src/2022/challenge-03.js
@@ -14,36 +14,25 @@ function distributeGifts(packOfGifts, reindeers) {
 
 // Expected: 2
 console.log(
-    // 12
-    // 24
-    // 
     distributeGifts(["book", "doll", "ball"], ["dasher", "dancer"])
 )
 
 // Expected: 1
 console.log(
-    // 3
-    // 4
     distributeGifts(['a', 'b', 'c'], ['d', 'e'])
 )
 
 // Expected: 0
 console.log(
-    // 17
-    // 8
     distributeGifts(['videogames', 'console'], ['midu'])
 )
 
 // Expected: 5
 console.log(
-    // 24
-    // 134
     distributeGifts(['game', 'videoconsole', 'computer'], ['midudev', 'pheralb', 'codingwithdani', 'carlosble', 'blasco', 'facundocapua', 'madeval', 'memxd'])
 )
 
 // Expected: 26
 console.log(
-    // 5
-    // 134
     distributeGifts(['music'], ['midudev', 'pheralb', 'codingwithdani', 'carlosble', 'blasco', 'facundocapua', 'madeval', 'memxd'])
 )
