cf51bcedfc243590aff06ed3cf71c7379fe75506
Flames <78381898+FlamesX-128@users.noreply.github.com>
2023-12-10 19:22:37 +0000

    Challenge 06 - Maybe more clear

--- 2023/challenge-06/main.ts
+++ 2023/challenge-06/main.ts
@@ -1,22 +1,17 @@
 function maxDistance(movements: string) {
+    const steps = [...movements];
     const movementValues: Record<string, number> = {
         '<': -1,
         '>': 1,
     };
 
-    const steps = [...movements];
-
     let a = 0;
     let b = 0;
 
     for (const step of steps) {
-        if (step !== '*') {
-            a += movementValues[step];
-            
-            continue
-        }
-
-        b++
+        step !== '*'
+            ? a += movementValues[step] 
+            : b++
     }
 
     return Math.abs(a) + b;
