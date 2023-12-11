fedab33c71344550beee4ccae293f5dd07858cec
Flames <78381898+FlamesX-128@users.noreply.github.com>
2023-12-11 22:06:38 +0000

    Challenge 10 - Cognitive complexity: 1

--- 2023/challenge-10/main.ts
+++ 2023/challenge-10/main.ts
@@ -1,4 +1,5 @@
-function createChristmasTree(ornaments: string, height: number): string {
+// More complexity, lower score, but more realistic.
+function _createChristmasTree(ornaments: string, height: number): string {
     const overflow = ornaments.length;
 
     let pointer = 0;
@@ -12,6 +13,28 @@ function createChristmasTree(ornaments: string, height: number): string {
         }
 
         tree += row.trimEnd() + '\n';
+    }
+
+    return tree += ' '.repeat(height - 1) + '|\n';
+}
+
+// Cognitive complexity: 1, Better Score
+function createChristmasTree(ornaments: string, height: number): string {
+    const overflow = ornaments.length;
+    let tree = '';
+  
+    let pointer = 0;
+    let i = 1;
+
+    for (const _ of ' '.repeat(height)) {
+        let row = ' '.repeat(height - i);
+
+        for (const _ of ' '.repeat(i)) {
+            row += ornaments[pointer++ % overflow] + ' '
+        }
+
+        tree += row.trimEnd() + '\n';
+        i++
     }
 
     return tree += ' '.repeat(height - 1) + '|\n';
